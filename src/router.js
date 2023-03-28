c679a54d83dea092892ef359656d92107894f5ed
cole <wangyiqing@up366.com>
2023-03-28 19:02:10 +0800

    feat: 路由模式修改

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import { canTurnTo } from '@/layout/util'
 import { getToken, removeToken } from '@/common/auth'
 import { LOGIN_NAME, HOME_NAME } from '@/config'
@@ -6,7 +6,7 @@ import useUserinfo from '@/store/userinfo'
 import routes from '@/routes'
 
 const router = createRouter({
-    history: createWebHashHistory(),
+    history: createWebHistory(),
     routes: routes
 })
 
